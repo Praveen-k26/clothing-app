527c8398af808773165232423b72828d7a21681f
Praveen-k26 <praveenn.ui@gmail.com>
2020-06-24 11:51:03 -0600

    Updated shop data with new data, updated cart utils to group items in the cart reducer

--- src/components/header/header.component .jsx
+++ src/components/header/header.component .jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 
 import './header.styles.scss';
 
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -28,12 +30,17 @@ const Header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             )}
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 );
-
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+//Instead of state we destructure nested values.
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
