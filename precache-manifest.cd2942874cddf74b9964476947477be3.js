03e53c73966c9f2fe11bd8086a331c1eae481f92
Praveen Koru <praveenkoru@Praveens-MacBook-Pro.local>
2021-02-01 09:35:38 -0700

    Updates

---
+++ precache-manifest.cd2942874cddf74b9964476947477be3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8891528dfc0ca334ea23de3c104a88e",
+    "url": "/clothing-app/index.html"
+  },
+  {
+    "revision": "23c8dc4f8e600faa1a0c",
+    "url": "/clothing-app/static/css/main.9700255e.chunk.css"
+  },
+  {
+    "revision": "c8debe2a00ee5c381994",
+    "url": "/clothing-app/static/js/2.d033d8fc.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/clothing-app/static/js/2.d033d8fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23c8dc4f8e600faa1a0c",
+    "url": "/clothing-app/static/js/main.bc4d3d6d.chunk.js"
+  },
+  {
+    "revision": "452cdd1c4a4211d46f3a",
+    "url": "/clothing-app/static/js/runtime-main.87e0cd40.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing-app/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "0783c3ec4759df8fe55d9b1495505940",
+    "url": "/clothing-app/static/media/pk.0783c3ec.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/clothing-app/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
